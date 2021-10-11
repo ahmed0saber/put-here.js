cbbad315e2e58fcbc55abbb9e1d750a431a2b66d
Ahmed Saber <64714761+ahmed0saber@users.noreply.github.com>
2021-10-11 09:55:10 +0200

    Update put-here.js

--- put-here.js
+++ put-here.js
@@ -1,22 +1,21 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    z = document.getElementsByClassName("put-here");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        file = elmnt.getAttribute("file");
-        if (file) {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    elmnt.removeAttribute("file");
-                    includeHTML();
+    var els, file, xhttp;
+    els = document.getElementsByClassName("put-here"); /*get all elements with this class*/
+    for (var i = 0; i < els.length; i++) { /*loop among them*/
+        file = els[i].getAttribute("file"); /*get file url*/
+        if (file) { /*if file found*/
+            xhttp = new XMLHttpRequest(); /*just abbreviation*/
+            xhttp.onreadystatechange = function() { /*on file changed or detected for first time*/
+                if (this.readyState == 4) { /*on file fully loaded*/
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;} /*Success => not performed in the first function call*/
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";} /*Failure*/
+                    els[i].removeAttribute("file"); /*remove the attribute not to load it again*/
+                    includeHTML(); /*again till the file is fully loaded*/
                 }
             }      
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            return;
+            xhttp.open("GET", file, true); /*prepare the request*/
+            xhttp.send(); /*send it*/
+            return; /*done*/
         }
     }
 };
